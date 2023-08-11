ca20f6ab959d218622dee2b951c734523f3171cd
iamvisithras <142045772+iamvisithras@users.noreply.github.com>
2023-08-11 23:49:08 +0530

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,27 @@
+a=int(input("Enter value  for a ")) #get input from run time
+b=a//500 #count for 500  (quo a/500)
+c=a%500 #remi a/500 => input for next demonination
+d=c//200 #200
+e=c%200
+f=e//100 #100
+g=e%100
+h=g//50 #50
+i=g%50
+j=i//20 #20
+k=i%20
+l=k//10 #10
+m=k%10
+n=m//5 #5
+o=m%5
+p=o//2 #2
+q=o%2
+r=q//1 #1
+print('FiveHundred',b)
+print('TwoHundred',d)
+print('Hundred',f)
+print('fifty',h)
+print('Twenty',j)
+print('Ten',l)
+print('Five',n)
+print('Two',p)
+print('One',r)
